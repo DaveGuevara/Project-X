e5ea51c6feccb7d9bf880e339f133c8245c52f0f
David Guevara <ideabox.by.dave@gmail.com>
2015-10-03 02:04:12 -0400

    added FB login

--- Website/js/form-validation-script.js
+++ Website/js/form-validation-script.js
@@ -5,18 +5,15 @@ var Script = function () {
     });
 
     $().ready(function () {
-        // validate the comment form when it is submitted
-        $("#feedback_form").validate();
-
         // validate signup form on keyup and submit
         $("#register_form").validate({
             rules: {
-                fullname: {
+                firstname: {
                     required: true,
                     minlength: 6
                 },
               
-                username: {
+                lastname: {
                     required: true,
                     minlength: 6
                 },
@@ -32,22 +29,18 @@ var Script = function () {
                 email: {
                     required: true,
                     email: true
-                },
-                topic: {
-                    required: "#newsletter:checked",
-                    minlength: 2
-                },
+                },               
                 agree: "required"
             },
             messages: {
-                fullname: {
-                    required: "Please enter a Full Name.",
-                    minlength: "Your Full Name must consist of at least 6 characters long."
+                firstname: {
+                    required: "Please enter a First Name.",
+                    minlength: "Your First Name must consist of at least 6 characters long."
                 },
        
-                username: {
-                    required: "Please enter a Username.",
-                    minlength: "Your username must consist of at least 5 characters long."
+                lastname: {
+                    required: "Please enter a Last Name.",
+                    minlength: "Your username must consist of at least 6 characters long."
                 },
                 password: {
                     required: "Please provide a password.",
@@ -58,33 +51,9 @@ var Script = function () {
                     minlength: "Your password must be at least 5 characters long.",
                     equalTo: "Please enter the same password as above."
                 },
-
                 email: "Please enter a valid email address.",
                 agree: "Please accept our terms & condition."
             }
-        });
-
-        // propose username by combining first- and lastname
-        $("#username").focus(function () {
-            var firstname = $("#firstname").val();
-            var lastname = $("#lastname").val();
-            if (firstname && lastname && !this.value) {
-                this.value = firstname + "." + lastname;
-            }
-        });
-
-        //code to hide topic selection, disable for demo
-        var newsletter = $("#newsletter");
-        // newsletter topics are optional, hide at first
-        var inital = newsletter.is(":checked");
-        var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
-        var topicInputs = topics.find("input").attr("disabled", !inital);
-        // show when newsletter is checked
-        newsletter.click(function () {
-            topics[this.checked ? "removeClass" : "addClass"]("gray");
-            topicInputs.attr("disabled", !this.checked);
-        });
+        });               
     });
-
-
 }();
