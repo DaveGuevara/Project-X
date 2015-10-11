e08df8a129cb8fd07b10c276d7e68113aa215ec9
Jhonny <jcorr3a@outlook.com>
2015-10-11 12:29:29 -0400

    made changes to the registration form and the js validator

--- Website/js/form-validation-script.js
+++ Website/js/form-validation-script.js
@@ -15,6 +15,18 @@ var Script = function () {
                     required: true,
                     minlength: 6
                 },
+                  username: {
+                    required: true,
+                    minlength: 6
+                },
+                 course: {
+                    required: true,
+                    minlength: 6
+                },
+                 section: {
+                    required: true,
+                    minlength: 6
+                },
                 password: {
                     required: true,
                     minlength: 5
@@ -27,8 +39,13 @@ var Script = function () {
                 email: {
                     required: true,
                     email: true
+                },
+                  agree: {
+                    required: true,
+                    agree: true
                 },               
                 agree: "required"
+               
             },
             messages: {
                 firstname: {
@@ -38,8 +55,9 @@ var Script = function () {
        
                 lastname: {
                     required: "Please enter a Last Name.",
-                    minlength: "Your username must consist of at least 6 characters long."
+                    minlength: "Your Last Name must consist of at least 6 characters long."
                 },
+               
                 password: {
                     required: "Please provide a password.",
                     minlength: "Your password must be at least 5 characters long."
@@ -49,8 +67,18 @@ var Script = function () {
                     minlength: "Your password must be at least 5 characters long.",
                     equalTo: "Please enter the same password as above."
                 },
+                   username: {
+                    required: "Please enter a Username.",
+                    minlength: "Your username must consist of at least 6 characters long."
+                },
+                   course: {
+                    required: "     Please enter your class.",
+                },
+                   section: {
+                    required: "    Please enter your section.",
+                },
                 email: "Please enter a valid email address.",
-                agree: "Please accept our terms & condition."
+                agree: "Please accept our terms & condition.",
             }
         });               
     });
