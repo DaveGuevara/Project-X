65705336c0919d14191c01b6c7babed856f95da0
emartinezv1992 <emartin4@ggc.edu>
2015-12-07 20:56:13 -0500

    Update logout.php

--- Website/logout.php
+++ Website/logout.php
@@ -1,19 +1,17 @@
 <?php
-include("connection.php");
-
-
- 
- mysqli_query($dbc, "INSERT INTO logins (UserGUID, SessionEnd) VALUES ([userid], CURRENT_TIMESTAMP)");
-
- 
- 
-header("location:login.php");
+//session_start();
+include('connection.php');
 
 session_start();
 session_unset();
-session_destroy();
-
-exit();
-
-
+if(session_destroy()) // Destroying All Sessions
+{
+			//echo "You've gone offline now!";
+			$update_status_query = mysql_query("INSERT INTO logins (UserGUID, SessionEnd) VALUES ([userid], CURRENT_TIMESTAMP)");
+		
+	
+		
+	// TAKES YOU TO INDEX.PHP if the session is destroyed
+	header("Location: login.php"); // Redirecting To Home Page
+}
 ?>
